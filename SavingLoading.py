08be28b92ce248ef38296915f93cfea7688e4bbf
ericburt1 <112107869+ericburt1@users.noreply.github.com>
2023-05-22 16:58:04 +0000

    Fixed inventory bugs and made it better

--- SavingLoading.py
+++ SavingLoading.py
@@ -4,8 +4,12 @@ class InfoToSave(object):
 
 	def __init__(self):
 		self.__inventory = []
+		self.__bagSize = 1 # bag sizes go from 1 - 10 you multiply the size by five to get the inventory size
 	
 	@property   # Just a random proprty setter just in case
 	def inventory(self):
 		return self.__inventory
 
+	@property   # Just a random proprty setter just in case
+	def bagSize(self):
+		return self.__bagSize
